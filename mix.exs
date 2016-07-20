1584021e24e03ba49a1a7cf1ef7fd17fcb0c23df
Arjan Scherpenisse <arjan@scherpenisse.net>
2016-07-20 10:26:40 +0200

    Update mix metadata

--- mix.exs
+++ mix.exs
@@ -12,22 +12,23 @@ defmodule NervesIoNfc.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp description do
+  """
+  Elixir access to LibNFC-compatible USB NFC readers
+  """
+  end
+
+  defp package do
+    %{files: ["lib", "src/*.[ch]", "mix.exs", "README.md", "LICENSE", "Makefile", "*.mk"],
+      maintainers: ["Arjan Scherpenisse"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/arjan/nerves_io_nfc"}}
+  end
+
   defp deps do
     [
       {:elixir_make, "~> 0.3"}
